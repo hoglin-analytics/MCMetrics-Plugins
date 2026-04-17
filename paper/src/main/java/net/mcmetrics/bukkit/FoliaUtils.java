2ae3c9e394d96146d808a8af136e4dbe72209e16
noidcoffee <himynameisalvinmooi@gmail.com>
2026-04-17 18:04:56 -0400

    Cache folia value
    Signed-off-by: noidcoffee <himynameisalvinmooi@gmail.com>

--- paper/src/main/java/net/mcmetrics/bukkit/FoliaUtils.java
+++ paper/src/main/java/net/mcmetrics/bukkit/FoliaUtils.java
@@ -3,12 +3,17 @@ package net.mcmetrics.bukkit;
 // https://docs.papermc.io/paper/dev/folia-support/
 public class FoliaUtils {
 
+    private static Boolean isFolia;
+
     public static boolean isFolia() {
-        try {
-            Class.forName("io.papermc.paper.threadedregions.RegionizedServer");
-            return true;
-        } catch (ClassNotFoundException e) {
-            return false;
+        if (isFolia == null) {
+            try {
+                Class.forName("io.papermc.paper.threadedregions.RegionizedServer");
+                isFolia = true;
+            } catch (ClassNotFoundException e) {
+                isFolia = false;
+            }
         }
+        return isFolia;
     }
 }
